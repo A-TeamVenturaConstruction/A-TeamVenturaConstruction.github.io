bf77be4da64aac913cddf97b838c08dac9e14a21
A-TeamVenturaConstruction <ateamventuraconstruction@gmail.com>
2018-06-28 19:18:16 -0500

    Add # line, slides and time change

--- JS-Script/homeScript.js
+++ JS-Script/homeScript.js
@@ -41,7 +41,7 @@ function autoSlides() {
             slideIndex = 1;
         }
         listOfSlides[slideIndex - 1].style.display = "block";
-        setTimeout(autoSlides, 4000);
+        setTimeout(autoSlides, 6000);
     } else {
         //starts manual slides
         startSlides(slideIndex);
