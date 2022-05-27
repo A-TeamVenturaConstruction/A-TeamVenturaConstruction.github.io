8f0ae27679e2d7346247a758c56e9b3de331cff4
A-TeamVenturaConstruction <40584276+A-TeamVenturaConstruction@users.noreply.github.com>
2022-05-27 13:46:41 -0500

    home add screen variable include in if

--- JS-Script/homeScript.js
+++ JS-Script/homeScript.js
@@ -1,6 +1,8 @@
-var screen_width = window.innerWidth;
-var screen_height = window.innerHeight;
+var browser_width = window.innerWidth;
+var browser_height = window.innerHeight;
 //document.getElementById("endPageBar").innerHTML = "Width: " + screen_width + " Height: " + screen_height;
+var screen_width = screen.width;
+var screen_height = screen.height;
 
 //if(screen_width )
 function show_menu()
@@ -22,7 +24,7 @@ function close_menu()
 function checkScreen()
 {
     'use strict';
-                        if(screen_width < 800)
+                        if((screen_width <= 800) || (browser_width <= 800))
                         {
                             //remove tabs
                             
@@ -31,29 +33,31 @@ function checkScreen()
                             document.getElementById("list_three").style.display = "none";
                             document.getElementById("list_four").style.display = "none";
                             //resize nav container
-                            document.getElementById("nav_container").style.paddingTop = "70px";
+                            document.getElementById("nav_container").style.paddingTop = "60px";
                             //resize image
-                            document.getElementById("logoImage").style.height = "60px";
+                            document.getElementById("logoImage").style.height = "50px";
                             //bar height
-                            document.getElementById("bar").style.height = "40px";
+                            document.getElementById("bar").style.height = "95px";
                             document.getElementById("menuImage").style.display = "block";
+                            //document.getElementById("experience_title").style.marginTop = "30px";
                             
                             
                         }
-                        if(screen_width > 800)
+                        if((screen_width > 800) && (browser_width > 800))
                         {
                             close_menu();
                             document.getElementById("menuImage").style.display = "none";
+                            //document.getElementByClassName("nav_list").style.display = "inline";
                              document.getElementById("list_one").style.display = "inline";
                             document.getElementById("list_two").style.display = "inline";
                             document.getElementById("list_three").style.display = "inline";
                             document.getElementById("list_four").style.display = "inline"; 
                             //resize nav container
-                            document.getElementById("nav_container").style.paddingTop = "90px";
+                            document.getElementById("nav_container").style.paddingTop = "70px";
                             //resize image
-                            document.getElementById("logoImage").style.height = "100px";
+                            document.getElementById("logoImage").style.height = "80px";
                             //bar height
-                            document.getElementById("bar").style.height = "8vw";
+                            document.getElementById("bar").style.height = "170px";
                            
                         }
 
